8724e3cc527560f7e5aada8322b95d6d32d6f1dc
Fayaz <flalani100@gmail.com>
2022-02-05 04:16:50 -0500

    Initialized server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { db } = require('./db/db.json')
+const PORT   = process.env.PORT || 3001;
+const app = express()
+// const apiRoutes = require('./routes/apiRoutes')
+// const htmlRoutes = require('./routes/htmlRoutes')
+
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+
+//parse incoming JSON data
+app.use(express.json());
+app.use(express.static('public'));
+// app.use('/api', apiRoutes);
+// app.use('/', htmlRoutes);
+
+
+app.post('/api/db', (req, res) => {
+    res.send('Hello!')
+})
+
+
+app.listen(PORT, () => console.log(`Server now on port ${PORT}!`))
