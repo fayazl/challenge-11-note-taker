667110f545ac74962fec9c8e1f31c92117c24e73
Fayaz <flalani100@gmail.com>
2022-02-05 06:36:05 -0500

    Added api route and referenced lib folder

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,19 @@
 const router = require("express").Router();
-const notes = require('../../db/db.json')
+const { notes } = require('../../db/db.json')
 const { newNote } = require('../../lib/notes')
 
 
-app.get('/notes', (req, res) => {
-    res.json(notes);
+router.get('/notes', (req, res) => {
+    let display = notes
+    res.json(display);
   });
+
+router.post('/notes', (req, res) => {
+  req.body.id = notes.length.toString();
+
+  const createNote = newNote(req.body, notes);
+
+  res.json(createNote)
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const {db} = require('./db/db.json')
 const PORT   = process.env.PORT || 3001;
 const app = express()
-// const apiRoutes = require('./routes/apiRoutes')
+const apiRoutes = require('./routes/apiRoutes')
 const htmlRoutes = require('./routes/htmlRoutes')
 
 
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 //parse incoming JSON data
 app.use(express.json());
 app.use(express.static('public'));
-// app.use('/api', apiRoutes);
+app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 
